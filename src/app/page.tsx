782ed9dd2f64f6306c52db31d71bef8cfc6c05ed
Ryan Legler <ryan.legler@gmail.com>
2024-01-06 14:43:18 -0800

    chore: adds a few other supported design props to `GitHubCalendar`

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,15 @@ import GitHubCalendar from 'react-github-calendar';
 
 export default function Home() {
     const colorScale = ['#00429d', '#1f58a6', '#376ead', '#ffa59e', '#dd4c65'];
+
     return (
         <main>
             <GitHubCalendar username='ryanlegler' />
             <GitHubCalendar
                 username='rttnbrgr'
+                blockMargin={2}
+                blockRadius={50}
+                blockSize={25}
                 theme={{
                     light: colorScale,
                     dark: colorScale,

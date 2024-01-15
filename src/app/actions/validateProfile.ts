c91c7ff35659aae09823820e57d94fa5ea60f6f0
Ryan Legler <ryan.legler@gmail.com>
2024-01-15 08:59:45 -0800

    feature: validateUser server action

---
+++ src/app/actions/validateProfile.ts
@@ -0,0 +1,35 @@
+'use server';
+
+import { UserNameInputState, userNameSchema } from '@/components/userNameInput/types';
+import { getProfile } from '@/dataLayer/getProfile';
+
+export async function validateProfile(_: any, formData: FormData): Promise<UserNameInputState> {
+    const userName = formData.get('userName');
+    const schemaResponse = userNameSchema.safeParse({ userName });
+
+    if (schemaResponse.success) {
+        const { userName } = schemaResponse?.data;
+        const profile = await getProfile(userName);
+        if ((profile as any)?.message === 'Not Found') {
+            return {
+                message: 'this github username is invalid ',
+                status: 'error',
+                data: '',
+            };
+        } else {
+            // we could just redirect here...
+            // redirect(`/user/${userName}`);
+            return {
+                message: 'All good in the hood',
+                status: 'success',
+                data: userName,
+            };
+        }
+    } else {
+        return {
+            message: 'input is invalid ',
+            status: 'error',
+            data: '',
+        };
+    }
+}

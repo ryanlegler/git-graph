ca7db68bcd0896295917b7a7e919cfeabc61684c
Ryan Legler <ryan.legler@gmail.com>
2024-01-15 12:39:34 -0800

    chore: park vs ark

--- src/stories/Button-panda.stories.tsx
+++ src/stories/Button-panda.stories.tsx
@@ -1,5 +1,5 @@
 import type { Meta, StoryObj } from '@storybook/react';
-import { css, cva } from 'styled-system/css';
+import { RecipeVariantProps, css, cva } from 'styled-system/css';
 import { styled } from 'styled-system/jsx';
 
 const meta = {
@@ -15,7 +15,7 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 // method 1
-export const Primary: Story = {
+export const PrimaryCSS: Story = {
     render: (args) => (
         <button
             className={css({
@@ -50,7 +50,7 @@ const flexClassNames = css({
     },
 });
 
-export const ExtractedStyles: Story = {
+export const ExtractedCSSStyles: Story = {
     render: () => <button className={flexClassNames}>Click Me</button>,
 };
 
@@ -73,6 +73,60 @@ const flexRecipe = cva({
 });
 const StyledButton = styled('div', flexRecipe);
 
-export const Styled: Story = {
+export const StyledCVA: Story = {
     render: () => <StyledButton>Click Me</StyledButton>,
+};
+
+// method 4
+const flexRecipeCVA = cva({
+    base: {
+        bg: 'github.400',
+        px: 3,
+        py: 2,
+        borderRadius: 'md',
+        color: 'white',
+        cursor: 'pointer',
+        _hover: {
+            bg: 'github.100',
+        },
+        _focus: {
+            bg: 'github.100',
+        },
+    },
+    variants: {
+        variant: {
+            primary: {
+                bg: 'github.400',
+                _hover: {
+                    bg: 'github.500',
+                },
+            },
+            secondary: {
+                bg: 'github.100',
+                borderWidth: '1px',
+                borderColor: 'github.400',
+                color: 'white',
+                _hover: {
+                    bg: 'github.200',
+                    borderColor: 'white',
+                },
+            },
+        },
+    },
+});
+export type ButtonVariants = RecipeVariantProps<typeof flexRecipeCVA>;
+const StyledButtonCVA = styled('div', flexRecipeCVA);
+
+type Story2 = StoryObj<ButtonVariants>;
+export const StyledCVARecipe: Story2 = {
+    argTypes: {
+        variant: {
+            options: ['primary', 'secondary'],
+            control: 'select',
+        },
+    },
+    render: (args: ButtonVariants) => <StyledButtonCVA {...args}> Click Me</StyledButtonCVA>,
+    args: {
+        variant: 'secondary',
+    },
 };

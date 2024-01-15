98ea6363242b105e77be8e67a664499c3d3a0552
Ryan Legler <ryan.legler@gmail.com>
2024-01-15 11:53:15 -0800

    chore: storybook uses tsconfig for path resolution

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,4 +1,5 @@
 import type { StorybookConfig } from '@storybook/nextjs';
+import TsconfigPathsPlugin from 'tsconfig-paths-webpack-plugin';
 
 const config: StorybookConfig = {
     stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
@@ -8,6 +9,18 @@ const config: StorybookConfig = {
         '@storybook/addon-onboarding',
         '@storybook/addon-interactions',
     ],
+    // this makes is so we extend the tsconfig.json file
+    webpackFinal: async (config) => {
+        if (config.resolve) {
+            config.resolve.plugins = [
+                ...(config.resolve.plugins || []),
+                new TsconfigPathsPlugin({
+                    extensions: config.resolve.extensions,
+                }),
+            ];
+        }
+        return config;
+    },
     framework: {
         name: '@storybook/nextjs',
         options: {},

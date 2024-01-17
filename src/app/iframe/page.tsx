d328c9b3e90e0b5d5a47f45ffd9dfee137a5371f
Ryan Legler <ryan.legler@gmail.com>
2024-01-16 23:15:20 -0800

    dummy route for testing the iframe - should probably just put in storybook instead

---
+++ src/app/iframe/page.tsx
@@ -0,0 +1,12 @@
+import { StyledFlex } from '@/components/ui/flex';
+
+export default async function IframeTest() {
+    return (
+        <StyledFlex direction='vertical' hAlign='center'>
+            <iframe
+                height='800px'
+                width='100%'
+                src='https://git-graph.vercel.app/embed/ryanlegler'></iframe>
+        </StyledFlex>
+    );
+}

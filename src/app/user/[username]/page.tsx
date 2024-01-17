27fe8fce7d3523f0fe46afca542d90412dc0b2d8
Nathan Rothenberger <nroth10@gmail.com>
2024-01-16 18:18:33 -0600

    chore: update metadata for user page

--- src/app/user/[username]/page.tsx
+++ src/app/user/[username]/page.tsx
@@ -4,12 +4,25 @@ import { getProfile } from '@/dataLayer/getProfile';
 import { getYears } from '@/dataLayer/getYears';
 import { StyledFlex } from '@/components/ui/flex';
 
-export default async function UserPage({ params: { username } }: { params: { username: string } }) {
+type Props = {
+    params: { username: string };
+};
+
+export async function generateMetadata({ params }: Props) {
+    return {
+        title: `${params.username} (GitGraph)`,
+    };
+}
+
+export default async function UserPage({ params: { username } }: Props) {
     const profile = await getProfile(username);
     const years = await getYears(username);
 
     return (
-        <StyledFlex direction='horizontal' hAlign='center'>
+        <StyledFlex direction='vertical' hAlign='center'>
+            {years.map((year) => (
+                <span key={year}>{year}</span>
+            ))}
             <GraphList years={years} username={username} avatarUrl={profile.avatar_url} />
         </StyledFlex>
     );

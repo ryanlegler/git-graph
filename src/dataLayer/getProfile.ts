ccb2bc5eed7a6c72be0d86f7047d577886982db1
Ryan Legler <ryan.legler@gmail.com>
2024-01-17 21:56:26 -0800

    infra: adds authentication to the github api call we use to get the profile url

--- src/dataLayer/getProfile.ts
+++ src/dataLayer/getProfile.ts
@@ -1,8 +1,10 @@
 import { User } from '@/app/types';
-
 export type UserResponse = User & { message: string };
+
 export async function getProfile(username: string) {
-    const dataFetch = await fetch(`https://api.github.com/users/${username}`);
+    const headers = new Headers();
+    headers.append('Authorization', `token ${process.env.GITHUB_TOKEN}`);
+    const dataFetch = await fetch(`https://api.github.com/users/${username}`, { headers });
     const profile: User = await dataFetch.json();
     return profile;
 }

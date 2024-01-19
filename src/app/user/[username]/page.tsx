9465470988cec1deff6674cf14c74cae39610e75
Ryan Legler <ryan.legler@gmail.com>
2024-01-19 00:26:03 -0800

    chore: wrap suspense around graphlist

--- src/app/user/[username]/page.tsx
+++ src/app/user/[username]/page.tsx
@@ -3,6 +3,7 @@ import { getProfile } from '@/dataLayer/getProfile';
 import { StyledFlex } from '@/components/ui/flex';
 import { Controls } from '@/components/controls';
 import { getContributions } from '@/dataLayer/getContributions';
+import { Suspense } from 'react';
 
 export default async function UserPage({ params: { username } }: { params: { username: string } }) {
     const profile = await getProfile(username);
@@ -11,11 +12,13 @@ export default async function UserPage({ params: { username } }: { params: { use
     return (
         <StyledFlex direction='vertical' hAlign='center'>
             <Controls />
-            <GraphList
-                contributions={contributions}
-                username={username}
-                avatarUrl={profile.avatar_url}
-            />
+            <Suspense fallback={<div>loading...</div>}>
+                <GraphList
+                    contributions={contributions}
+                    username={username}
+                    avatarUrl={profile.avatar_url}
+                />
+            </Suspense>
         </StyledFlex>
     );
 }

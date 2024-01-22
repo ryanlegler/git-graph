e164487e30a6a42004310810342953503e7b3832
Ryan Legler <ryan.legler@gmail.com>
2024-01-21 23:51:34 -0800

    feature: helper to get the day of week string from the "Day" number

---
+++ src/components/controls/hooks/useDayString.ts
@@ -0,0 +1,15 @@
+import { useMemo } from 'react';
+
+// use the hook in a "use client" component
+export function useDayString(day: Day) {
+    return useMemo(() => {
+        return getDayString(day);
+    }, [day]);
+}
+
+// this can be used server side if we ever want to pre-render this information
+export function getDayString(day: Day) {
+    const date = new Date();
+    date.setDate(day);
+    return date.toLocaleString('en-US', { weekday: 'long' });
+}

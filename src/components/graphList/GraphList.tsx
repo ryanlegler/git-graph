4bf142569d63c44c5fb9c77c9cffbb3dad281f97
Ryan Legler <ryan.legler@gmail.com>
2024-01-21 23:49:17 -0800

    bug: ended up running into hydration error with the ActivityCalendar component - use workaround to only render on the client

--- src/components/graphList/GraphList.tsx
+++ src/components/graphList/GraphList.tsx
@@ -1,39 +1,40 @@
-/* eslint-disable @next/next/no-img-element */
 'use client';
-import ActivityCalendar, { Props as ActivityCalendarProps } from 'react-activity-calendar';
-import { css } from 'styled-system/css';
 
+/* eslint-disable @next/next/no-img-element */
+import { useEffect, useState } from 'react';
 import Link from 'next/link';
+import { css } from 'styled-system/css';
+import { useAtomValue } from 'jotai/react';
+import { optionsAtom, renderYearsAtom } from '@/atoms';
+
+import ActivityCalendar, { Props as ActivityCalendarProps } from 'react-activity-calendar';
+
 import { StyledFlex } from '@components/ui/flex';
-import { useMemo } from 'react';
-import { hideColorLegendAtom } from '@/atoms';
-import { useAtom } from 'jotai';
 import { Contributions } from '@/dataLayer/getContributions';
 
 export function GraphList({
     username,
     avatarUrl,
     contributions,
-    hideColorLegend: hideColorLegendStatic, // this static value is picked up by params in the /user routed as passed as props
 }: {
     contributions: Contributions;
     username: string;
     avatarUrl: string;
 } & Omit<ActivityCalendarProps, 'data'>) {
-    const [hideColorLegend] = useAtom(hideColorLegendAtom);
+    // I Started getting hydration errors - not sure why it wasn't happening before.
+    // This component is marked as "use client" but the component but next will still pre-rendering it on the server.
+    // This is a hack to get around it for now, as recommended here:
+    // https://nextjs.org/docs/messages/react-hydration-error
+    const [isClient, setIsClient] = useState(false);
+    useEffect(() => {
+        setIsClient(true);
+    }, []);
 
-    // we use the static value if it's passed in, otherwise we use the dynamic value from the atom
-    const resolvedHideColorLegend = hideColorLegendStatic || hideColorLegend;
+    const controlsOptions = useAtomValue(optionsAtom);
+    const years = useAtomValue(renderYearsAtom) || [];
 
-    const years = useMemo(
-        () =>
-            Object.keys(contributions)
-                .map((year) => Number(year))
-                .reverse(),
-        [contributions]
-    );
-
-    const colorScale = ['#00429d', '#1f58a6', '#376ead', '#ffa59e', '#dd4c65'];
+    const colorScaleDark = ['#00429d', '#1f58a6', '#376ead', '#ffa59e', '#dd4c65'];
+    const colorScaleLight = ['#f9ed69', '#f08a5d', '#b83b5e', '#6a2c70', '#3c1642'];
 
     return (
         <StyledFlex direction='vertical' gap={6}>
@@ -56,25 +57,30 @@ export function GraphList({
                     className={css({
                         fontSize: 30,
                         fontWeight: 700,
-                    })}>
+                    })}
+                >
                     {username}
                 </Link>
             </StyledFlex>
 
-            {years.map((year) => (
+            {years?.map((year) => (
                 <div key={year}>
                     <h3
                         className={css({
                             fontSize: 18,
                             fontWeight: 900,
-                        })}>
+                        })}
+                    >
                         {year}
                     </h3>
-                    <ActivityCalendar
-                        theme={{ light: colorScale, dark: colorScale }}
-                        data={contributions[year]}
-                        hideColorLegend={resolvedHideColorLegend}
-                    />
+
+                    {isClient ? (
+                        <ActivityCalendar
+                            theme={{ light: colorScaleLight, dark: colorScaleDark }}
+                            data={contributions[year]}
+                            {...controlsOptions}
+                        />
+                    ) : null}
                 </div>
             ))}
         </StyledFlex>

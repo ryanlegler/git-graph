5cf8ee466433068c2e0469648a9d15b9cfb51726
Nathan Rothenberger <nroth10@gmail.com>
2024-01-23 18:38:55 -0600

    wip

--- src/components/controls/yearsSelect/YearsSelect.tsx
+++ src/components/controls/yearsSelect/YearsSelect.tsx
@@ -2,83 +2,98 @@ import * as Select from '@/components/ui/Select';
 import { Box, Flex, Grid } from 'styled-system/jsx';
 import { CheckIcon, ChevronsUpDownIcon } from 'lucide-react';
 import { YearsSelectProps } from './types';
+import { useState } from 'react';
 
 export function YearsSelect(props: YearsSelectProps) {
-    // type Item = { label: string; value: string; disabled?: boolean };
-    // const [_, setSelectedItems] = useState<Item[]>([]);
+    type Item = { label: string; value: string; disabled?: boolean; years: number[] };
+    const [selectedItem, setSelectedItem] = useState<Item>();
 
     if (!props.years) {
         return <div />;
     }
 
     const rangeItems = [
-        { label: 'This Year', id: [props.years[0]], value: 'this-year' },
-        { label: 'Last Year', id: [props.years[1]], value: 'last-year' },
-        { label: 'All Years', id: props.years, value: 'all-years' },
+        {
+            label: 'This Year',
+            value: 'this-year',
+            years: [props.years[0]],
+        },
+        {
+            label: 'Last Year',
+            value: 'last-year',
+            years: [props.years[1]],
+        },
+        {
+            label: 'All Years',
+            value: 'all-years',
+            years: props.years,
+        },
     ];
 
     const individualItems = props.years.map((year) => {
         return {
             label: year.toString(),
-            value: [year],
-            id: `${year.toString()}`,
+            value: year.toString(),
+            years: [year],
         };
     });
 
-    const mergeItems = [...rangeItems, ...individualItems];
-
-    // console.log('mergeItems', mergeItems);
+    const mergeItems: Item[] = [...rangeItems, ...individualItems];
 
     return (
-        <Select.Root
-            positioning={{ sameWidth: true }}
-            width='2xs'
-            // uses item.value internally as a key 🚨 Dear Arc, please add this to your docs 🤦‍♂️
-            items={mergeItems}
-            bg='red.5'
-            color='lime'
-            onValueChange={(e) => {
-                console.log(e.items);
-                console.log(e);
-            }}
-        >
-            <Select.Label style={{ display: 'none' }}>Framework</Select.Label>
-            <Select.Control>
-                <Select.Trigger>
-                    <Select.ValueText />
-                    <ChevronsUpDownIcon />
-                </Select.Trigger>
-            </Select.Control>
-            <Select.Positioner>
-                <Select.Content style={{ background: 'black' }}>
-                    {/*
-                     */}
-                    <Select.ItemGroup id='rangeItems'>
-                        {rangeItems.map((item, i) => (
-                            <Select.Item key={i} item={item}>
-                                <Select.ItemText>{item.label}</Select.ItemText>
-                                <Select.ItemIndicator>
-                                    <CheckIcon />
-                                </Select.ItemIndicator>
-                            </Select.Item>
-                        ))}
-                    </Select.ItemGroup>
-                    <Box w='100' h='1px' py='1' px='2'>
-                        <Box h='1px' bg='lime' />
-                    </Box>
-
-                    <Select.ItemGroup id='individualItems'>
-                        {individualItems.map((item, i) => (
-                            <Select.Item key={i} item={item}>
-                                <Select.ItemText>{item.label}</Select.ItemText>
-                                <Select.ItemIndicator>
-                                    <CheckIcon />
-                                </Select.ItemIndicator>
-                            </Select.Item>
-                        ))}
-                    </Select.ItemGroup>
-                </Select.Content>
-            </Select.Positioner>
-        </Select.Root>
+        <div>
+            <div>hi</div>
+            <div>
+                state:
+                {selectedItem && selectedItem.years.map((x) => <div key={x}>{x}</div>)}
+            </div>
+            <Select.Root
+                positioning={{ sameWidth: true }}
+                width='2xs'
+                // uses item.value internally as a key 🚨 Dear Arc, please add this to your docs 🤦‍♂️
+                items={mergeItems}
+                onValueChange={(e) => {
+                    const thisItem = e.items[0] as Item;
+                    setSelectedItem(thisItem);
+                }}
+            >
+                <Select.Label style={{ display: 'none' }}>Framework</Select.Label>
+                <Select.Control>
+                    <Select.Trigger>
+                        {/* @ts-expect-error: Ark Select Value Text not propoerly typed */}
+                        <Select.ValueText placeholder='Select one' />
+                        <ChevronsUpDownIcon />
+                    </Select.Trigger>
+                </Select.Control>
+                <Select.Positioner>
+                    <Select.Content>
+                        <Select.ItemGroup id='rangeItems'>
+                            {rangeItems.map((item, i) => (
+                                <Select.Item key={i} item={item}>
+                                    <Select.ItemText>{item.label}</Select.ItemText>
+                                    <Select.ItemIndicator>
+                                        <CheckIcon />
+                                    </Select.ItemIndicator>
+                                </Select.Item>
+                            ))}
+                        </Select.ItemGroup>
+                        <Box w='100' h='1px' py='1' px='1'>
+                            {/* i do not understand why fg, fg-default, foreground, forground, or any equivilent does not work */}
+                            <Box h='1px' backgroundColor='gray.a3' />
+                        </Box>
+                        <Select.ItemGroup id='individualItems'>
+                            {individualItems.map((item, i) => (
+                                <Select.Item key={i} item={item}>
+                                    <Select.ItemText>{item.label}</Select.ItemText>
+                                    <Select.ItemIndicator>
+                                        <CheckIcon />
+                                    </Select.ItemIndicator>
+                                </Select.Item>
+                            ))}
+                        </Select.ItemGroup>
+                    </Select.Content>
+                </Select.Positioner>
+            </Select.Root>
+        </div>
     );
 }

d36f0e84acda1a5041d32cef7d9837d5ff774506
Ryan Legler <ryan.legler@gmail.com>
2024-02-07 07:54:20 -0800

    change back casing

--- src/components/controls/sliderControl/SliderControl.tsx
+++ src/components/controls/sliderControl/SliderControl.tsx
@@ -1,10 +1,10 @@
-import React from "react";
-import { FormField, FormItem } from "@/components/ui/form";
-import { Label } from "@/components/ui/label";
+import React from 'react';
+import { FormField } from '@/components/ui/form';
+import { Label } from '@/components/ui/label';
 
 // types
-import { SliderControlProps } from "./types";
-import { Slider } from "@/components/ui/slider";
+import { SliderControlProps } from './types';
+import { Slider } from '@/components/ui/slider';
 
 function SliderControl({ formKey, form, max, min, step = 1 }: SliderControlProps) {
     return (
@@ -12,10 +12,10 @@ function SliderControl({ formKey, form, max, min, step = 1 }: SliderControlProps
             control={form.control}
             name={formKey}
             render={({ field }) => (
-                <div className="flex flex-col gap-2">
+                <div className='flex flex-col gap-2'>
                     <Label htmlFor={formKey}>{formKey}</Label>
                     <Slider
-                        className="w-full"
+                        className='w-full'
                         id={formKey}
                         defaultValue={[field.value]}
                         max={max}

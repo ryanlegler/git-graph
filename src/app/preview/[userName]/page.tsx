6aaa18140f3860e2cf64de859391dcd56bf040f5
Ryan Legler <ryan.legler@gmail.com>
2024-02-15 21:56:03 -0800

    username

--- src/app/preview/[userName]/page.tsx
+++ src/app/preview/[userName]/page.tsx
@@ -6,14 +6,14 @@ export default async function Preview({
     params,
     searchParams,
 }: {
-    params: { userName: string };
+    params: { username: string };
     searchParams: SearchParamOptions;
 }) {
-    const { userName } = params;
+    const { username } = params;
     const headersList = headers();
     const domain = headersList.get('host') || '';
     const paramsString = new URLSearchParams(getHydratedSearchParams(searchParams)).toString();
-    const url = `http://${domain}/embed/${userName}?${paramsString}`;
+    const url = `http://${domain}/embed/${username}?${paramsString}`;
     return (
         <div className='flex gap-10 flex-col justify-center items-center h-screen'>
             <h1 className='text-3xl font-bold'>Preview</h1>

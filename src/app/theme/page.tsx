35cdc63dd98519f9a96d4a83935a815c19943432
Nathan Rothenberger <nroth10@gmail.com>
2024-02-27 19:23:26 -0600

    wip: btn sheet

--- src/app/theme/page.tsx
+++ src/app/theme/page.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import tailwindConfig from '../../../tailwind.config';
+import { Button } from '@/components/ui/button';
 const config: Config = tailwindConfig;
+import './styles.css';
+import { IconButton } from '@/components/ui/icon-button';
+import { Cross2Icon, ReloadIcon } from '@radix-ui/react-icons';
 
 interface ColorObject {
     DEFAULT?: string;
@@ -50,14 +54,44 @@ export default function ColorSheet() {
         []
     );
     return (
-        <div className='grid grid-cols-5 gap-10 p-7'>
-            {resolvedColors.map((color, index) => (
-                <div key={index} className='flex gap-2 flex-col'>
-                    <div className='h-24 rounded-lg' style={{ backgroundColor: color.value }}></div>
-                    <p className='text-xs font-mono'>Name: {color.name}</p>
-                    <p className='text-xs font-mono'>Value: {color.value}</p>
+        <div className='o'>
+            <div className='grid grid-cols-5 gap-10 p-7'>
+                {resolvedColors.map((color, index) => (
+                    <div key={index} className='flex gap-2 flex-col'>
+                        <div
+                            className='h-24 rounded-lg'
+                            style={{ backgroundColor: color.value }}
+                        ></div>
+                        <p className='text-xs font-mono'>Name: {color.name}</p>
+                        <p className='text-xs font-mono'>Value: {color.value}</p>
+                    </div>
+                ))}
+            </div>
+            <div className='space-y-2 p-7'>
+                <h2>Buttons</h2>
+                <h3>Variants</h3>
+                <div className='flex gap-2 pb-2'>
+                    <Button variant='default'>Button</Button>
+                    <Button variant='secondary'>Button</Button>
+                    <Button variant='ghost'>Button</Button>
                 </div>
-            ))}
+                <h3>Sizes</h3>
+                <div className='flex gap-2 pb-2'>
+                    <Button>Button</Button>
+                    <Button size='sm'>Button</Button>
+                    <Button size='lg'>Button</Button>
+                    <Button size='icon'>Button</Button>
+                </div>
+                <h3>Icon Button</h3>
+                <div className='flex gap-2 pb-2'>
+                    <Button size='icon'>
+                        <ReloadIcon className='h-4 w-4' />
+                    </Button>
+                    <IconButton>
+                        <ReloadIcon className='h-4 w-4' />
+                    </IconButton>
+                </div>
+            </div>
         </div>
     );
 }

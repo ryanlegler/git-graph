418818ff24e35473fc2ecebb2da471aecfe533bb
Nathan Rothenberger <nroth10@gmail.com>
2024-02-27 19:23:14 -0600

    add + extract icon btn

---
+++ src/components/ui/icon-button.tsx
@@ -0,0 +1,24 @@
+import * as React from 'react';
+import { Slot } from '@radix-ui/react-slot';
+import { cva, type VariantProps } from 'class-variance-authority';
+
+import { cn } from '@/lib/utils';
+import { _disabled, _display, _focus, _ghost, _icon, _rest, _size, _typography } from './button';
+
+const buttonVariants = cva([_display, _typography, _focus, _disabled, _rest, _icon, _ghost]);
+
+export interface ButtonProps
+    extends React.ButtonHTMLAttributes<HTMLButtonElement>,
+        VariantProps<typeof buttonVariants> {
+    asChild?: boolean;
+}
+
+const IconButton = React.forwardRef<HTMLButtonElement, ButtonProps>(
+    ({ className, asChild = false, ...props }, ref) => {
+        const Comp = asChild ? Slot : 'button';
+        return <Comp className={cn(buttonVariants({ className }))} ref={ref} {...props} />;
+    }
+);
+IconButton.displayName = 'Icon Button';
+
+export { IconButton, buttonVariants };
